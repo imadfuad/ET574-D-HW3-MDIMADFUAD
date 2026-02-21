2c04e39add054b4614fd2da443252cf5a89356bc
imadfuad <imaduddin778@gmail.com>
2026-02-21 07:48:43 +0000

    Task D - fixed index bug (Jon to John)

--- 1_copilot.py
+++ 1_copilot.py
@@ -2,6 +2,8 @@
 students = ["Jon", "Kim", "Lee"]
 students.append("Sara")
 students.append("Miko") 
+# change Jon to John
+students[0] = 'John'
 def greet_students(students): 
     for student in students: 
         print(f"Hello, {student}!")
