9fd143a4996c1e883e79819f3e7c4f9fb9d3c1a2
imadfuad <imaduddin778@gmail.com>
2026-02-21 07:38:28 +0000

    Task C - added Sara, Miko and total count

--- 1_copilot.py
+++ 1_copilot.py
@@ -1,7 +1,10 @@
 # list of three students named Jon, Kim and Lee
 students = ["Jon", "Kim", "Lee"]
+students.append("Sara")
+students.append("Miko") 
 def greet_students(students): 
     for student in students: 
         print(f"Hello, {student}!")
 greet_students(students) 
+print("Total students:", len(students))
  
